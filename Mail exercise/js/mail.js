aed609ccb40534a8835aaf77b0b3bcbcf8a758b3
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-04-18 20:32:56 +0200

    feat(js):add email sistem security

--- Mail exercise/js/mail.js
+++ Mail exercise/js/mail.js
@@ -10,13 +10,35 @@ myLink.addEventListener('click', (event) => {
 // // function to run when button is clicked
 document.getElementById("btn").addEventListener("click", function() {
 
-    // get the email from the input
+    // user email
     let email = document.getElementById("email").value;
-    console.log("The email is", email);
+    console.log("The user email is", email);
+    if (email == "") {
+        alert("Please insert a email");
+    } else if (email.indexOf("@") == -1) {
+        alert("Please insert a email with @");
+    }
+    
+    // cancel the value email after click on the button
+    document.getElementById("email").value = "";
+
+    document.getElementById("printEmailUser").innerHTML = email;
+    if (email == "") {
+        document.getElementById("printEmailUser").innerHTML = "No email inserted";
+    } else if (email.indexOf("@") == -1) {
+        document.getElementById("printEmailUser").innerHTML = "No valid email";
+    } 
+    document.getElementById("printEmailUser").style.fontSize = "20px";
 
     // list of emails
-    let emails = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email4@gmail.com", "email5@gmail.com", "email6@gmail,com"];
+    let emails = ["email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "email4@gmail.com", "email5@gmail.com", "email6@gmail.com"];
     console.log("The list of emails is", emails);
+    if (email != emails) {
+        document.getElementById("printEmailUser").innerHTML = "email is not in the list";
+    }
+    document.getElementById("emails").innerHTML = emails;
+    document.getElementById("emails").style.fontSize = "20px";
+
     
     // creat  a loop to check if the email is in the list or not
     let found = false;
@@ -29,10 +51,12 @@ document.getElementById("btn").addEventListener("click", function() {
     // print the result
     if (found) {
         console.log("Welcome to our website, yor email is in the list", email);
-        alert("Welcome to our website");
+        alert("Welcome to our website, your email is in the list");
     } else {
         console.log("Sorry, you are not allowed to enter our website, your email is not in the list", email);
-        alert("Sorry, you are not allowed to enter our website");
-    }
+        alert("Sorry, you are not allowed to enter our website, your email is not in the list");
+    } 
 
 });
+
+// function when the user click on the button
